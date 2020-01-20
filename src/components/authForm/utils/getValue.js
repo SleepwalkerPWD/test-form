07cccfdaeac073e10263341de7982a248bf8b5a0
SleepwalkerPWD <37107643+SleepwalkerPWD@users.noreply.github.com>
2020-01-20 23:02:31 +0300

    fix

--- src/components/authForm/utils/getValue.js
+++ src/components/authForm/utils/getValue.js
@@ -1,4 +1,4 @@
-//Значение из обьекта ошибок для реализоации error border
+//Значение из обьекта ошибок для реализоации error border и ошибки под input
 const getValue = (object, val) => {
   for (let value in object) {
     if(val === value)
