64c3c40f5999c18b465b5ba306ef5517173a086b
SleepwalkerPWD <37107643+SleepwalkerPWD@users.noreply.github.com>
2020-01-22 01:04:25 +0300

    fix

--- src/components/authForm/constants/index.js
+++ src/components/authForm/constants/index.js
@@ -1,12 +1,10 @@
 import { INPUT_PROPS } from './inputForm';
-import { LIST_ITEMS } from './listItems'; 
 import { INITIAL_VALUES } from './initialValue';
 import { VALID_EMAIL_REGEXP } from './regexpEmail';
 import { COUNTRY_ARR } from './country';
 
 export {
   INPUT_PROPS,
-  LIST_ITEMS,
   INITIAL_VALUES,
   VALID_EMAIL_REGEXP,
   COUNTRY_ARR
